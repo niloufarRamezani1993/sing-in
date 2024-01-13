8058c09100b61542543ed7c9b2c89a4934623211
Niloufar Ramazeni <niloufarramezani7@gmail.com>
2024-01-14 02:44:41 +0330

    im confused

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    colors:{
+      backGRCL :'rgba(19 19 24 / 0.4)',
+    
+      blueSingIn :'#0b6bcb',
+      whiteFont: {
+       default : '#f0f4f8',
+       googleBtn: '#dde7ee',
+      },
+      FooterClr : '#9fa6ad',
+      bgBlack:{
+        btnBlack : '#171a1c',
+      },
+      borderCl:{
+        bg:'#0b0d0e',
+        line:'#32383e',
+      },
+      orCL : '#9fa6ad',
+
+    },
+   
+    
+    extend: {
+      fontFamily:{
+        'sens' : ['Inter']
+      }
+    },
   },
   plugins: [],
 }
